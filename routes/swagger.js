ef248e8b23aca75913dc381ff164f9de3eae439e
sichali395 <lsichali@byupathway.edu>
2026-05-30 14:42:18 +0200

    Fix: Correct swagger routing engine implementation

--- routes/swagger.js
+++ routes/swagger.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');
 
+// Serves the interactive Swagger UI interface assets
 router.use('/', swaggerUi.serve);
+
+// Configures Swagger UI to parse the compiled swagger.json document
 router.get('/', swaggerUi.setup(swaggerDocument));
 
 module.exports = router;
